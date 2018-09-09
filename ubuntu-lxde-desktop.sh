25f87872a9b6ed12eb9cf2427b7331c87ecf1d75
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-09-09 22:44:52 +0000

    Update ubuntu-lxde-desktop.sh

--- ubuntu-lxde-desktop.sh
+++ ubuntu-lxde-desktop.sh
@@ -1,14 +1,18 @@
 sudo apt-get update
 sudo apt-get install -y terminator chromium-browser actionaz firefox tightvncserver lxde
-wget https://downloads.vivaldi.com/stable/vivaldi-stable_1.15.1147.52-1_amd64.deb
-sudo dpkg -i vivaldi*
 sudo sh -c 'wget -O - http://deb.opera.com/archive.key | apt-key add -'
 echo 'deb http://deb.opera.com/opera/ stable non-free' > /etc/apt/sources.list.d/opera.list
 sudo add-apt-repository ppa:alessandro-strada/ppa
-sudo apt-get update sudo apt-get install -y opera qupzilla midori google-drive-ocamlfuse
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install -y opera qupzilla midori google-drive-ocamlfuse
+sublime-text
 vncserver
 steelers
 steelers
 sudo apt-get remove -y tightvncserver
 sudo apt-get install -y vnc4server
+wget https://downloads.vivaldi.com/stable/vivaldi-stable_1.15.1147.52-1_amd64.deb
+sudo dpkg -i vivaldi*
 
